fee4cf5a8cfa21825ac221a4a1ec46f17cff1808
Anne Whitman <awhitman@mountain.com>
2023-11-29 12:19:41 -0500

    chapters 15-16 (auth & metadata)

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,17 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from "@/app/lib/data";
+import { Metadata } from 'next';
+
+// Don't do this. Would have to do this on every page and change everywhere if the company name changed.
+// export const metadata: Metadata = {
+//     title: 'Invoices | Acme Dashboard',
+// };
+
+// Instead use template in root layout file and do this:
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
 
 export default async function Page({searchParams}: {
     searchParams?: {
